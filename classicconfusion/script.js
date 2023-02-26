fb1e92ba344be24810e5a20584465cc2015f0cb9
Josh <joshua@esplinfamily.com>
2023-02-26 15:42:13 -0700

    added Lost Soul and cool effects

--- classicconfusion/script.js
+++ classicconfusion/script.js
@@ -1,6 +1,32 @@
 var isPlaying = false;
 var source = null;
 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h1").onmouseover = event => {
+    let i = 0;
+
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => {
+            if(index === 7) {
+                return " ";
+            }
+            if(index < i) {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+    if (i >= event.target.dataset.value.length){
+        clearInterval(interval);
+    }
+
+    i += 1 / 3;
+    }, 30);
+}
+
 // add each song from a folder named 'unfinished_songs' to the table
 function playSwitch() {
     if (isPlaying) {
@@ -24,5 +50,6 @@ function choose(song) {
     }
     document.getElementById("player").pause();
     document.getElementById("play_button").innerHTML = "Play";
+    document.getElementById("currSong").innerHTML = song;
     isPlaying = false;
 }
